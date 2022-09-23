c5fce7f317dc06144d766c678418a5fc73f9bece
Mike Starina <x@pnhd.ru>
2022-09-23 16:29:10 +0300

    mobile optimized

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const heightInput = document.querySelector('#height');
 const qtyInput = document.querySelector('#qty');
 const submitButton = document.querySelector('form__submit-button');
 const form = document.querySelector('form');
-const result = document.querySelector('#calculations__result');
 const params = document.querySelector('#parameters');
 const error = document.querySelector('#error');
+
+const filmTotalField = document.querySelector('#filmTotal');
+const filmPriceField = document.querySelector('#filmPrice');
+const printPriceField = document.querySelector('#printPrice');
+const totalPriceField = document.querySelector('#totalPrice');
 
 const dtfWidth = 50;
 
@@ -35,10 +39,15 @@ form.addEventListener('submit', (e) => {
                 const filmPrice = orderHeightInMeters * 2000;
                 const printPrice = qty * 150;
                 const totalPrice = filmPrice + printPrice;
-                console.log(totalPrice);
+                //console.log(totalPrice);
 
-                result.textContent = `>> ${orderHeightInMeters} м. / ${filmPrice}р. печать / ${printPrice}р. перенос / ${totalPrice}р итого`;
+               
                 params.textContent = `ш: ${width - 1}см. в: ${height - 1}см. кол-во: ${qty} шт.`;
+
+                filmTotalField.textContent = `${orderHeightInMeters}м.`;
+                filmPriceField.textContent = `${filmPrice}р. печать`;
+                printPriceField.textContent = `${printPrice}р. перенос`;
+                totalPriceField.textContent = `${totalPrice}р. итого`;
 
             } else {
 
@@ -47,8 +56,14 @@ form.addEventListener('submit', (e) => {
                 const totalPrice = filmPrice + printPrice;
                 console.log(totalPrice);
 
-                result.textContent = `>> ${orderHeightInMeters} м. / ${filmPrice}р. печать / ${printPrice}р. перенос / ${totalPrice}р итого`;;
+                
                 params.textContent = `ш: ${width - 1}см. в: ${height - 1}см. кол-во: ${qty} шт.`;
+
+                filmTotalField.textContent = `${orderHeightInMeters}м.`;
+                filmPriceField.textContent = `${filmPrice}р. печать`;
+                printPriceField.textContent = `${printPrice}р. перенос`;
+                totalPriceField.textContent = `${totalPrice}р. итого`;
+
             }
 
         } else if (width > dtfWidth && height < dtfWidth) {
@@ -64,8 +79,14 @@ form.addEventListener('submit', (e) => {
                 const totalPrice = filmPrice + printPrice;
                 console.log(totalPrice);
 
-                result.textContent = `>> ${orderHeightInMeters} м. / ${filmPrice}р. печать / ${printPrice}р. перенос / ${totalPrice}р итого`;
+                
                 params.textContent = `ш: ${width - 1}см. в: ${height - 1}см. кол-во: ${qty} шт.`;
+
+                filmTotalField.textContent = `${orderHeightInMeters}м.`;
+                filmPriceField.textContent = `${filmPrice}р. печать`;
+                printPriceField.textContent = `${printPrice}р. перенос`;
+                totalPriceField.textContent = `${totalPrice}р. итого`;
+
 
             } else {
 
@@ -74,8 +95,14 @@ form.addEventListener('submit', (e) => {
                 const totalPrice = filmPrice + printPrice;
                 console.log(totalPrice);
 
-                result.textContent = `>> ${orderHeightInMeters} м. / ${filmPrice}р. печать / ${printPrice}р. перенос / ${totalPrice}р итого`;;
+                
                 params.textContent = `ш: ${width - 1}см. в: ${height - 1}см. кол-во: ${qty} шт.`;
+
+                filmTotalField.textContent = `${orderHeightInMeters}м.`;
+                filmPriceField.textContent = `${filmPrice}р. печать`;
+                printPriceField.textContent = `${printPrice}р. перенос`;
+                totalPriceField.textContent = `${totalPrice}р. итого`;
+
             }
 
         } else {
